bbd4d3055f83b9120087d47929b93ec350629153
PritamMore93 <60602689+PritamMore93@users.noreply.github.com>
2020-03-10 17:17:23 +0530

    Update Sample.java

--- src/Sample.java
+++ src/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 		System.out.println("Hello");
 		System.out.println("Hello1");
 		System.out.println("Hello3");
-		System.out.println("Hello4");
+		System.out.println("Hello5");
 		}
 
 }
